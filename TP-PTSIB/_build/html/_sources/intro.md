4a17e744f2257934ec467a6827cda982ac1b1bed
Darkwell <julien.stiker@me.com>
2021-09-07 18:56:02 +0200

    Intro en rst

--- TP-PTSIB/_build/html/_sources/intro.md
+++ TP-PTSIB/_build/html/_sources/intro.md
@@ -1,3 +1,7 @@
 # Les TP d'informatique en PTSI-B au lycée Coufignal
 
+```{image} logo.png
+:width: 1466px
+:align: center
+```
 
---
+++ TP-PTSIB/_build/jupyter_execute/essai.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+# 
+# # Markdown content is written as regular markdown
+# 
+# You can also write {ref}`MyST Markdown <myst>`.
+# 
+# ```{code-cell}
+# ---
+# cellmetadatakey: val1
+# ---
+# print("Here is a Python cell")
+# ```
+# 
+# And here is more markdown.
+# 
+# +++
+# 
+# Separate markdown cells with `+++` lines.
+# 
+# ```{code-cell}
+# :cellmetadatakey: val1
+# print("Another code cell with a second optional metadata syntax")
+# ```
+# 
+# 
+# # Les TP d'informatique en PTSI-B au lycée Coufignal
+# 
+# ```{image} logo.png
+# :width: 1466px
+# :align: center
+# ```
+# 
+# 
+# ```{code-cell}
+# print(2)
+# ```
+
+# 
+
+# In[ ]:
+
+
+
+
